adc7859b97eb3a69c30bfd21920b41ec4223613c
Casper <asilbek2001.05@gmail.com>
2024-08-11 15:32:55 +0500

    fix some errors

--- src/main/java/org/example/figma/config/AuditorAwareImpl.java
+++ src/main/java/org/example/figma/config/AuditorAwareImpl.java
@@ -3,7 +3,6 @@ package org.example.figma.config;
 import lombok.RequiredArgsConstructor;
 import org.example.figma.entity.User;
 import org.example.figma.repo.UserRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +10,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AuditorAwareImpl {
     private final UserRepository userRepository;
-    @Bean
+
     public User getAuthenticatedUser() {
         String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userRepository.findByEmail(email);
--- src/test/java/org/example/figma/FigmaApplicationTests.java
+++ src/test/java/org/example/figma/FigmaApplicationTests.java
@@ -3,11 +3,9 @@ package org.example.figma;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FigmaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
